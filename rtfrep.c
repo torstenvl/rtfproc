bc2b6bf65a6d5c647ca2f2deb2e9ec586dfd6394
Lee Ockert <torstenvl@gmail.com>
2019-12-21 01:37:40 -0500

    Reimplement with an object-based design
    This design uses an object-based design to better track files, streams,
    buffers, and iterators, as well as state information about the current
    processing of the RTF file.

--- rtfrep.c
+++ rtfrep.c
@@ -2,91 +2,217 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include "rtfrep.h"
 
-#define ISERROR(x)        ((x) != 0)
+// REMINDER OF STRUCTURE DECLARATIONS AND CONSTANTS
+// typedef struct repobj {
+//     FILE *is;             // Input stream
+//     FILE *os;             // Output stream
+//     byte ib[FB_Z];        // Input file buffer
+//     byte ibto[FB_Z];      // Input file buffer token oracle (readahead)
+//     byte ob[FB_Z];        // Output file buffer
+//     uccp pb[PB_Z];        // Pattern buffer for comparing codepoints
+//     size_t ibi;           // Input buffer iterator
+//     size_t ibtoi;         // Input buffer token oracle iterator
+//     size_t obi;           // Output buffer iterator
+//     size_t pbi;           // Pattern buffer iterator
+//     size_t ibz;           // Input buffer actual size
+//     size_t obz;           // Output buffer actual size
+//     size_t pbz;           // Pattern buffer actual size
+//     size_t pbmaps[PB_Z];  // Maps to start of rtf sequence for pb[i] in ib[]
+//     size_t pbmape[PB_Z];  // Maps to end of rtf sequence for pb[i] in ib[]
+// } repobj;
+// FB_Z file buffer (ib and ob) size
+// PB_Z pattern buffer size
 
-#define FILENAME          "test.rtf"
-#define FILE_BUFFER_SIZE  512
-#define ORACLE_BUFFER_SIZE 128
 
-int main(void) {
+int rtf_process(repobj *r) {
+    int nerrs;  // Number of errors
+    int err;    // Current error
 
-  // Variable declarations
-  char filebuffer[FILE_BUFFER_SIZE];
-  char oraclebuffer[ORACLE_BUFFER_SIZE];
-  FILE  *in_stream;
-  FILE  *out_stream = stdout;
-  size_t readsize;
-  int filestatus = 0;
-  int errflags = 0;
+    nerrs = 0;
 
-  size_t i;
-  size_t oracle;
-  size_t stackdepth = 0;
-  size_t maxstackdepth = 0;
+    while (!feof(r->is)) {
 
-  // Open the file
-  in_stream = fopen(FILENAME, "rb");
+        memset_s(r->ob, FB_Z, 0, FB_Z);
+        r->ibz = fread(r->ib, 1, FB_Z, r->is);
 
-  if (NULL == in_stream) {
-    fprintf(stderr, "Unable to open file '%s' for reading.\n", FILENAME);
-    exit(EXIT_FAILURE);
-  }
-
-  while (!feof(in_stream)) {
-
-    readsize = fread(filebuffer, 1, FILE_BUFFER_SIZE, in_stream);
-
-    // If we can't fill the buffer, we either have an error or have reached
-    // the end of the file.
-    if (readsize < FILE_BUFFER_SIZE) {
-      filestatus = ferror(in_stream);
-      if (ISERROR(filestatus)) {
-        // If it's an error, log it and move on.
-        fprintf(stderr, "File read error %d!\n", filestatus);
-        errflags++;
-      } else {
-        // End of the file was reached.  We might still have some good data
-        // though, so we will keep processing/writing.
-      }
-    }
-
-    for (i = 0; i < readsize; i++) {
-      if (filebuffer[i] == '\\') {
-        // The next thing we have is an RTF code of some sort
-        // The oracle cursor looks into the near future.  Start just past i
-        // and read until we reach the end of the RTF code token.
-        // RTF code tokens are terminated by a backslash, whitespace, or
-        // the start or end of an RTF block (delimited by { and }).
-        // FRAGILE:  WHAT IF WE EXHAUST A BUFFER?
-        memset(oraclebuffer, 0, ORACLE_BUFFER_SIZE);
-        for (oracle=1;
-             oracle < (ORACLE_BUFFER_SIZE-1) && oracle+i < readsize;
-             oracle++) {
-          if (filebuffer[i+oracle] == '\\') break;
-          if (isspace(filebuffer[i+oracle])) break;
-          if (filebuffer[i+oracle] == '}') break;
-          if (filebuffer[i+oracle] == '{') break;
-          if (oraclebuffer[0] == '\'' && oracle > 3) break;
-          oraclebuffer[oracle-1] = filebuffer[i+oracle];
+        // Incomplete buffer fill = error or EOF. If an error, log it.
+        // Then keep processing. (If EOF, it'll fall through on next while().)
+        if (r->ibz < FB_Z) {
+            if (ISERROR(err = ferror(r->is))) {
+                fprintf(stderr, "File read error %d!\n", err);
+                nerrs++;
+            }
         }
-        fprintf(stderr, "Found RTF command token: %s\n", oraclebuffer);
-      }
-      if (filebuffer[i] == '{') {
-        stackdepth++;
-        if (stackdepth > maxstackdepth) maxstackdepth = stackdepth;
-      }
-      if (filebuffer[i] == '}') {
-        if (stackdepth == 0) fprintf(stderr, "TRYING TO DECREASE STATE STRUCT STACK DEPTH BELOW ZERO\n\n");
-        stackdepth--;
-      }
+
+        for (r->ibi = 0, r->obi = 0; r->ibi < r->ibz; r->ibi++) {
+            // if (r->ib[r->ibi] == '\\') { }
+            // if (r->ib[r->ibi] == '{') { }
+            // if (r->ib[r->ibi] == '}') { }
+            r->ob[r->obi++] = r->ib[r->ibi];
+        }
+
+        fwrite(r->ob, 1, r->obi, r->os);
+
+    } // end of buffer-read loop
+
+    return nerrs;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/***************************************************************************/
+/*                                                                         */
+/*  THE FOLLOWING SECTION IMPLEMENTS BASIC OBJECT CREATION / DESTRUCTION   */
+/*                                                                         */
+/*  repobj *new_repobj(void);                                              */
+/*  repobj *new_repobj_from_file(const char *);                            */
+/*  repobj *new_repobj_to_file(const char *);                              */
+/*  repobj *new_repobj_from_file_to_file(const char *, const char *);      */
+/*  void destroy_repobj(repobj *);                                         */
+/*                                                                         */
+/***************************************************************************/
+
+
+
+
+
+repobj *new_repobj(void) {
+    return new_repobj_from_stream_to_stream(stdin, stdout);
+}
+
+
+
+
+
+repobj *new_repobj_from_file(const char *ifname) {
+    FILE *ifptr;
+    repobj *r;
+
+    ifptr = fopen(ifname, "r");
+    if (!ifptr) return NULL;
+
+    r = new_repobj_from_stream_to_stream(ifptr, stdout);
+    if (!r) {
+        fclose (ifptr);
+        return NULL;
     }
 
-    fwrite(filebuffer, 1, readsize, out_stream);
+    return r;
+}
 
-  } // end of buffer-read loop
 
-  fprintf(stderr, "\n\n");
 
-  return errflags;
+
+
+repobj *new_repobj_to_file(const char *ofname) {
+    FILE *ofptr;
+    repobj *r;
+
+    ofptr = fopen(ofname, "w");
+    if (!ofptr) return NULL;
+
+    r = new_repobj_from_stream_to_stream(stdin, ofptr);
+    if (!r) {
+        fclose(ofptr);
+        return NULL;
+    }
+
+    return r;
+}
+
+
+
+
+
+repobj *new_repobj_from_file_to_file(const char *ifname, const char *ofname) {
+    FILE *ifptr;
+    FILE *ofptr;
+    repobj *r;
+
+    ifptr = fopen(ifname, "r");
+    if (!ifptr) return NULL;
+
+    ofptr = fopen(ofname, "w");
+    if (!ofptr) {
+        fclose(ifptr);
+        return NULL;
+    }
+
+    r = new_repobj_from_stream_to_stream(ifptr, ofptr);
+    if (!r) {
+        fclose(ifptr);
+        fclose(ofptr);
+        return NULL;
+    }
+
+    return r;
+}
+
+
+
+
+
+repobj *new_repobj_from_stream_to_stream(FILE *ifstream, FILE *ofstream) {
+    repobj *r;
+
+    r = malloc(sizeof(*r));
+    if (!r) return NULL;
+
+    memset_s(r, sizeof *r, 0, sizeof *r);
+    r->is = ifstream;
+    r->os = ofstream;
+
+    return r;
+}
+
+
+
+
+
+void destroy_repobj(repobj *r) {
+    if (r->is != stdin) fclose(r->is);
+    if (r->os != stdout) fclose(r->os);
+
+    memset_s(r, sizeof *r, 0, sizeof *r);
+
+    free(r);
 }
