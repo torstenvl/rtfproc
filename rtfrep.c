e440d97bb4e1ba69a8f7600cf3b58309ca49bf9a
Lee Ockert <torstenvl@gmail.com>
2021-04-07 17:23:47 -0400

    Redesign with simpler buffers & state machine
    Previous designs were overly complicated in design and engaged in
    premature optimization by buffering input and output in a way that was
    difficult to track and reason about. The focus on getting things fast
    before even knowing what was performing slowly was naive, and hampered
    progress in getting them working first. "Anything worth doing is worth
    doing poorly." Getting it done is better than getting it perfect.
    
    This design is intentionally simple. We will not buffer I/O within the
    program.
    
            - Operate on STDIN / STDOUT only.
            - No internal buffering.  Can add this later if necessary.
            - Only buffers will be raw, command, and text buffers
    
    This is going to require at least a simple state machine. We might as
    well start implementing one. First iteration uses txt, cmd, cmdproc
    as read states.

--- rtfrep.c
+++ rtfrep.c
@@ -1,247 +1,173 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
+#include <stdbool.h>
 #include "rtfrep.h"
-
-// REMINDER OF STRUCTURE DECLARATIONS AND CONSTANTS
-// typedef struct repobj {
-//     FILE*  is;            // Input stream
-//
-//     byte   ib[FB_Z];      // Input file buffer
-//     size_t ibz;           // Input buffer actual size
-//     size_t ibi;           // Input buffer iterator
-//
-//     FILE*  os;            // Output stream
-//
-//     byte   ob[FB_Z];      // Output file buffer
-//     size_t obz;           // Output buffer actual size
-//     size_t obi;           // Output buffer iterator
-//
-//     byte   ibto[FB_Z];    // Input file buffer token oracle (readahead)
-//     size_t ibtoi;         // Input buffer token oracle iterator
-//
-//     uccp   pb[PB_Z];      // Pattern buffer for comparing codepoints
-//     size_t pbz;           // Pattern buffer actual size
-//     size_t pbi;           // Pattern buffer iterator
-//
-//     size_t pbmaps[PB_Z];  // Maps to start of rtf sequence for pb[i] in ib[]
-//     size_t pbmape[PB_Z];  // Maps to end of rtf sequence for pb[i] in ib[]
-//
-// } repobj;
-// FB_Z file buffer (ib and ob) size
-// PB_Z pattern buffer size
+#include "memzero.h"
 
 
-int rtf_process(repobj *r) {
-    int nerrs;  // Number of errors
-    int err;    // Current error
+void resetstate(rtfobj *R) {
+    R->rdstt = txt;
+    R->mtchstt = indeterminate;
+    memzero(R->rawbfr, RAW_BUFFER_SIZE);
+    memzero(R->txtbfr, TXT_BUFFER_SIZE);
+    memzero(R->cmdbfr, CMD_BUFFER_SIZE);
+    R->ri = 0L;
+    R->ti = 0L;
+    R->ci = 0L;
+    R->c = '\0';
+}
 
-    nerrs = 0;
 
-    while (!feof(r->is)) {
+int rtf_replace(rtfobj *R) {
+    resetstate(R);
 
-        ////////////////////////
-        // SET UP OUR BUFFERS //
-        ////////////////////////
-        memzero(r->ob, FB_Z);
-        r->ibz = fread(r->ib, 1, FB_Z, r->is);
-        r->ibi = 0;
-        r->obi = 0;
-
-        /////////////////////////////////////////////////////
-        // CHECK FOR INCOMPLETE BUFFER FILL → ERROR OR EOF //
-        /////////////////////////////////////////////////////
-        if (r->ibz < FB_Z) {
-            if (ISERROR(err = ferror(r->is))) {
-                fprintf(stderr, "File read error %d!\n", err);
-                nerrs++;
+    while ((R->c = fgetc(R->ipstrm)) != EOF) {
+        R->rawbfr[R->ri++] = R->c;
+        switch (R->c) {
+        case '{':
+            if (R->rdstt == cmd) { R->rdstt = cmdproc; }
+            // New scope. Copy and push rtf attribute set.
+            break;
+        case '}':
+            if (R->rdstt == cmd) { R->rdstt = cmdproc; }
+            // End of scope. Restore previous rtf attribute set.
+            break;
+        case '\\':
+            if ((R->ri >= 2) && (R->rawbfr[R->ri - 2] == '*')) {
+                // Allow novel RTF commands to continue
+                R->cmdbfr[R->ci++] = R->c;
+            } else if (R->rdstt == cmd) {
+                R->rawbfr[--R->ri] = '\0';
+                ungetc(R->c, R->ipstrm);
+                R->rdstt = cmdproc;
             } else {
-                // Incomplete buffer fill and no error
-                // → must be end of file. Let it fall through
-                // on the while() loop and end gracefully.
+                R->rdstt = cmd;
+                R->cmdbfr[R->ci++] = R->c;
+            }
+            break;
+        case '\t':
+        case '\n':
+        case '\v':
+        case '\f':
+        case '\r':
+        case ' ':
+            if (R->rdstt == cmd) { R->rdstt = cmdproc; }
+            else { R->txtbfr[R->ti++] = R->c; }
+            break;
+        default:
+            if (R->rdstt == cmd) { R->cmdbfr[R->ci++] = R->c; }
+            else if (R->rdstt == txt) { R->txtbfr[R->ti++] = R->c; }
+        }
+
+
+        if (R->rdstt == cmd) {
+            // Don't do anything until we read the whole command in
+            continue;
+        } else if (R->rdstt == cmdproc) {
+            // Let us know what command we encountered, do something if
+            // appropriate, and otherwise move on.
+            fprintf(stderr, "Encountered command '%s'\n", R->cmdbfr);
+            //***************************************************************//
+            // TODO                                                          //
+            // With the current setup, we are resetting buffers after read-  //
+            // ing a command.  That ensures that we are always outputting    //
+            // the commands we receive outside of a match, but it ALSO means //
+            // we are losing our 'place' with text chunks if there's an RTF  //
+            // command in the middle of a word.  This could lead to missed   //
+            // replacements -- indeed, it almost definitely will.            //
+            // What we need to do instead is to reset ONLY the command       //
+            // reading buffer HERE... and also, if we're starting to read    //
+            // a potential match, we need to output the raw buffers before   //
+            // then as well.  What we really want to have happen is this:    //
+            //     - complete command with no match text                     //
+            //          - output the raw buffer and reset                    //
+            //     - complete command with partial match text                //
+            //          - hold off until we know whether to output rep       //
+            // The best way to do this might be to distinguish between a     //
+            // readstate of txt versus a readstate of unknown.  Use txt as   //
+            // a readstate only when we're actually matching text that may   //
+            // need to be replaced, and unknown in all other circumstances.  //
+            // However, to do that, we will also need to be able to restore  //
+            // from a cmd readstate to the previous readstate of either txt  //
+            // or unknown.  A full 'stack' of readstates seems overkill.  I  //
+            // will have to think about it some more.  In the meantime, here //
+            // is what I'm thinking for code in this particular place:       //
+            //***************************************************************//
+            // Do something with it
+            // dosomething(rtf_cmd_buffer);
+            // And set us up to keep chugging
+            // memzero(rtf_cmd_buffer, CMD_BUFFER_SIZE);
+            // ci = 0L;
+            // readstate = txt;
+            // NOTE NOTE NOTE: Another option would be to output the command
+            // buffer when we're done reading it, but I'm not sure I like that
+            // because we could mess up non-command rtf tokens like { or }
+            fwrite(R->rawbfr, sizeof(byte), R->ri, R->opstrm);
+            resetstate(R);
+        } else if (R->rdstt == txt) {
+            // If we are reading in text, let's see if we have a match or
+            // we aren't sure yet.
+            //     Sure we have nomatch --> flush buffers
+            //     Sure we have a match --> write out the replacement
+            //     Not sure we have one --> just keep reading
+            R->mtchstt = nomatch;
+            for (R->di = 0; R->dict[R->di] != NULL && R->mtchstt != match; R->di += 2) {
+                // Check for ANY partial match at length ti
+                if (!strncmp(R->dict[R->di], R->txtbfr, R->ti)) {
+                    // Check for a full match.  If they match so far and this
+                    // iterator position is the full length of the match
+                    // token in the dictionary, then it's a full match.
+                    if (strlen(R->dict[R->di]) == R->ti) {
+                        R->mtchstt = match;
+                        break;
+                    } else {
+                        R->mtchstt = indeterminate;
+                    }
+                }
+            }
+            if (R->mtchstt == indeterminate) {
+                // If we've hit our testing limits, there's no supported match
+                if (R->ci + 1 == CMD_BUFFER_SIZE) R->mtchstt = nomatch;
+                if (R->ri + 1 == RAW_BUFFER_SIZE) R->mtchstt = nomatch;
+                if (R->ti + 1 == TXT_BUFFER_SIZE) R->mtchstt = nomatch;
+                // Otherwise we will just keep chugging along
+            }
+            if (R->mtchstt == nomatch) {
+                fwrite(R->rawbfr, sizeof(byte), R->ri, R->opstrm);
+                resetstate(R);
+            }
+            if (R->mtchstt == match) {
+                fwrite(R->dict[R->di+1], sizeof(byte), strlen(R->dict[R->di+1]), R->opstrm);
+                resetstate(R);
             }
         }
+    }
+    fwrite(R->rawbfr, sizeof(byte), R->ri, R->opstrm);
 
-        ///////////////////////////////////////////////////////////////
-        // LOOP INPUT BUFFER TO OUTPUT BUFFER, PROCESSING ON THE WAY //
-        ///////////////////////////////////////////////////////////////
-        while (r->ibi < r->ibz) {
-            // if (r->ib[r->ibi] == '\\') { }
-            // if (r->ib[r->ibi] == '{') { }
-            // if (r->ib[r->ibi] == '}') { }
-            r->ob[r->obi++] = r->ib[r->ibi++];
-        }
-
-        /////////////////////////////
-        // FLUSH THE OUTPUT BUFFER //
-        /////////////////////////////
-        fwrite(r->ob, 1, r->obi, r->os);
-
-    } // end of buffer-read loop
-
-    return nerrs;
-}
-
-
-
-void memzero(void *const p, const size_t n)
-{
-  volatile unsigned char *volatile p_ = (volatile unsigned char *volatile) p;
-  size_t i = (size_t) 0U;
-
-  while (i < n) p_[i++] = 0U;
+    return 0;
 }
 
 
 
 
+rtfobj *new_rtfobj_stream_stream_dict(FILE *in, FILE *out, char **dict) {
+    rtfobj *R = malloc(sizeof *R);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/***************************************************************************/
-/*                                                                         */
-/*  THE FOLLOWING SECTION IMPLEMENTS BASIC OBJECT CREATION / DESTRUCTION   */
-/*                                                                         */
-/*  repobj *new_repobj(void);                                              */
-/*  repobj *new_repobj_from_file(const char *);                            */
-/*  repobj *new_repobj_to_file(const char *);                              */
-/*  repobj *new_repobj_from_file_to_file(const char *, const char *);      */
-/*  void destroy_repobj(repobj *);                                         */
-/*                                                                         */
-/***************************************************************************/
-
-
-
-
-
-repobj *new_repobj(void) {
-    return new_repobj_from_stream_to_stream(stdin, stdout);
-}
-
-
-
-
-
-repobj *new_repobj_from_file(const char *ifname) {
-    FILE *ifptr;
-    repobj *r;
-
-    ifptr = fopen(ifname, "r");
-    if (!ifptr) return NULL;
-
-    r = new_repobj_from_stream_to_stream(ifptr, stdout);
-    if (!r) {
-        fclose (ifptr);
-        return NULL;
+    if (R) {
+        R->rdstt = txt;
+        R->mtchstt = indeterminate;
+        memzero(R->rawbfr, RAW_BUFFER_SIZE);
+        memzero(R->txtbfr, TXT_BUFFER_SIZE);
+        memzero(R->cmdbfr, CMD_BUFFER_SIZE);
+        R->ri = 0L;
+        R->ti = 0L;
+        R->ci = 0L;
+        R->c = '\0';
+        R->ipstrm = in;
+        R->opstrm = out;
+        R->dict = dict;
     }
 
-    return r;
-}
-
-
-
-
-
-repobj *new_repobj_to_file(const char *ofname) {
-    FILE *ofptr;
-    repobj *r;
-
-    ofptr = fopen(ofname, "w");
-    if (!ofptr) return NULL;
-
-    r = new_repobj_from_stream_to_stream(stdin, ofptr);
-    if (!r) {
-        fclose(ofptr);
-        return NULL;
-    }
-
-    return r;
-}
-
-
-
-
-
-repobj *new_repobj_from_file_to_file(const char *ifname, const char *ofname) {
-    FILE *ifptr;
-    FILE *ofptr;
-    repobj *r;
-
-    ifptr = fopen(ifname, "r");
-    if (!ifptr) return NULL;
-
-    ofptr = fopen(ofname, "w");
-    if (!ofptr) {
-        fclose(ifptr);
-        return NULL;
-    }
-
-    r = new_repobj_from_stream_to_stream(ifptr, ofptr);
-    if (!r) {
-        fclose(ifptr);
-        fclose(ofptr);
-        return NULL;
-    }
-
-    return r;
-}
-
-
-
-
-
-repobj *new_repobj_from_stream_to_stream(FILE *ifstream, FILE *ofstream) {
-    repobj *r;
-
-    r = malloc(sizeof(*r));
-    if (!r) return NULL;
-
-    memzero(r, sizeof *r);
-    r->is = ifstream;
-    r->os = ofstream;
-
-    return r;
-}
-
-
-
-
-
-void destroy_repobj(repobj *r) {
-    if (r->is != stdin) fclose(r->is);
-    if (r->os != stdout) fclose(r->os);
-
-    memzero(r, sizeof *r);
-
-    free(r);
+    return R;
 }
